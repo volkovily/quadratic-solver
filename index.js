5a55302dbcd4d7c4bc5669601209eb2193d1357c
Ilya <volkov.ilyya@gmail.com>
2023-03-06 13:18:38 +0100

    Revert "added check if a is 0"
    This reverts commit a9a2bc1591b87dfa674f6fd72c69cebceb322b61.

--- index.js
+++ index.js
@@ -17,31 +17,26 @@ function getInput(prompt, callback) {
 }
 
 getInput("a = ", (a) => {
-  if (a === "0") {
-    console.log("Error. a cannot be 0");
-    interface.close();
-  } else {
-    getInput("b = ", (b) => {
-      getInput("c = ", (c) => {
-        console.log(`The equation is: (${a})x^2 + (${b})x + (${c}) = 0`);
+  getInput("b = ", (b) => {
+    getInput("c = ", (c) => {
+      console.log(`The equation is: (${a})x^2 + (${b})x + (${c}) = 0`);
 
-        const discriminant = b ** 2 - 4 * a * c;
+      const discriminant = b ** 2 - 4 * a * c;
 
-        if (discriminant > 0) {
-          const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
-          const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+      if (discriminant > 0) {
+        const x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        const x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
-          console.log(`There are 2 roots:\nx1 = ${x1}\nx2 = ${x2}`);
-        } else if (discriminant === 0) {
-          const x = -b / (2 * a);
+        console.log(`There are 2 roots:\nx1 = ${x1}\nx2 = ${x2}`);
+      } else if (discriminant === 0) {
+        const x = -b / (2 * a);
 
-          console.log(`There is 1 root:\nx = ${x}`);
-        } else {
-          console.log("There are 0 roots.");
-        }
+        console.log(`There is 1 root:\nx = ${x}`);
+      } else {
+        console.log("There are 0 roots.");
+      }
 
-        interface.close();
-      });
+      interface.close();
     });
-  }
+  });
 });
